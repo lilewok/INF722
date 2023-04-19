76c7559fadce45cca332b9a4d54d55eb8acf0022
lilewok <131092780+lilewok@users.noreply.github.com>
2023-04-18 20:10:42 -0400

    Added Comments, Added Space to Punctuation removal, New Files from Olivia

--- main.R
+++ main.R
@@ -4,36 +4,41 @@ library(tidytext)
 #Read in file 
 mtt <- read_file("data/MyTalkTools/MyTalkTools-About.txt")
 
+#output file to console 
+#cat(mtt)
 
-cat(mtt)
+#Remove punctuation and replace with space
+#also removes extra white space
+mtt <- str_replace_all(mtt, "\\s+", " ") %>% 
+  str_replace_all(., "\\p{Punct}", " ")  
 
-mtt <- str_replace_all(mtt, "\\s+", " ") %>% # Remove extra white spaces
-  str_replace_all(., "\\p{Punct}", "")  # Remove punctuations
-
-
-
+#places data into data frame table
 mtt_df <- tibble(mtt)
 
+#creating unigrams
 tidy_mtt <- mtt_df %>% unnest_tokens(word,mtt)
 
-str(mtt_df)
-
+#loading dataset called stop_words
 data("stop_words")
 
-custom_stop_words <- bind_rows(tibble(word = c("app","apps"),
+#include custom stop words
+custom_stop_words <- bind_rows(tibble(word = c("app","apps","apple"),
                                       lexicon = c("custom")),
                                stop_words)
 
+#removing all stop words
 tidy_mtt <- tidy_mtt %>% anti_join(custom_stop_words)
 
+#word count
 tidy_mtt %>% count(word, sort = TRUE) 
 
+#frequency graph
 tidy_mtt %>%
   count(word, sort = TRUE) %>%
   filter(n > 1) %>%
   mutate(word = reorder(word, n)) %>%
   ggplot(aes(n, word)) +
-  geom_col() +n 
+  geom_col() +
   labs(y = NULL)
 
 
