ef67f2d824b29d1950f372505f0e2cfa7bd02446
lilewok <131092780+lilewok@users.noreply.github.com>
2023-04-22 18:42:54 -0400

    Completed the MTT_ALL
    The MTT_All program pulls in the about and review text and will create unigram, trigram, and bigram csv's and plots.  I am removing the restriction of how many individual values are plotted

--- MTT_All.R
+++ MTT_All.R
@@ -1,168 +1,117 @@
 library(tidyverse)
 library(tidytext)
 
-#Read in file 
-mtt <- read_file("data/MyTalkTools/MyTalkTools-About.txt")
+###Read in files###
+    mtt <- read_file("data/MyTalkTools/MyTalkTools-About.txt")
+  # set the directory containing the review files
+    file_dir <- "data/MyTalkTools/reviews/"
+  # get a list of file names in the directory
+    file_paths <- list.files(file_dir, full.names = TRUE)
+    
+###Create files###
+    # set the file path for the output file
+      output_file <- "data/MyTalkTools/mtt_about.csv"
+      output_file2 <- "data/MyTalkTools/mtt_about_2.csv"
+      output_file3 <- "data/MyTalkTools/mtt_about_3.csv"
+      output_file4 <- "data/MyTalkTools/mtt_reviews.csv"
+      output_file5 <- "data/MyTalkTools/mtt_reviews2.csv"
+      output_file6 <- "data/MyTalkTools/mtt_reviews3.csv"
+      
+###Cleanup About Files###
+  #Remove punctuation and replace with space
+  #also removes extra white space
+      mtt <- str_replace_all(mtt, "\\s+", " ") %>% 
+        str_replace_all(., "\\p{Punct}", " ")  
+  #places about data into data frame table
+      mtt_df <- tibble(mtt)
+  #loading dataset called stop_words
+      data("stop_words")
+  #include custom stop words
+      custom_stop_words <- bind_rows(tibble(word = c("app","apps","apple","ipad","aac","touchchat","proloquo2go","ve",
+                                                     "mytalktools","mytalk","don","too","iphone","day","pts","lot","lot",
+                                                     "itunes","it's","i've","ipod","2","000","20","10","3","5"),
+                                            lexicon = c("custom")),
+                                     stop_words)
+###Process Data###      
+      tidy_mtt <- mtt_df %>% unnest_tokens(word,mtt) 
+      tidy_mtt <- tidy_mtt %>% anti_join(custom_stop_words)
+      write_csv(tidy_mtt, output_file)
+      tidy_mtt2 <- mtt_df %>% unnest_tokens(word,mtt, token = "ngrams", n= 2) 
+      tidy_mtt2 <- tidy_mtt2 %>% anti_join(custom_stop_words)
+      write_csv(tidy_mtt2, output_file2)
+      tidy_mtt3 <- mtt_df %>% unnest_tokens(word,mtt, token = "ngrams", n= 3)
+      tidy_mtt3 <- tidy_mtt3 %>% anti_join(custom_stop_words)
+      write_csv(tidy_mtt3, output_file3) 
 
-#Remove punctuation and replace with space
-#also removes extra white space
-mtt <- str_replace_all(mtt, "\\s+", " ") %>% 
-  str_replace_all(., "\\p{Punct}", " ")  
+  # read in the files and combine the data
+    mtt_reviews <- map(file_paths, read_file) %>% 
+    # combine the text into a single character vector
+      reduce(c) %>% 
+    # convert to a data frame
+      tibble(text = .) %>% 
+    # separate the text into words
+      unnest_tokens(word, text)
+    # read in the files and combine the data
+      mtt_reviews2 <- map(file_paths, read_file) %>% 
+    # combine the text into a single character vector
+      reduce(c) %>% 
+    # convert to a data frame
+      tibble(text = .) %>% 
+    # separate the text into words
+      unnest_tokens(word, text, token = "ngrams", n= 2)
 
-#places data into data frame table
-mtt_df <- tibble(mtt)
+      mtt_reviews3 <- map(file_paths, read_file) %>% 
+    # combine the text into a single character vector
+      reduce(c) %>% 
+    # convert to a data frame
+      tibble(text = .) %>% 
+    # separate the text into words
+      unnest_tokens(word, text, token = "ngrams", n= 3)
+  
+    #removing all stop words
+      mtt_reviews <- mtt_reviews %>% anti_join(custom_stop_words)
+      mtt_reviews2 <- mtt_reviews2 %>% anti_join(custom_stop_words)
+      mtt_reviews3 <- mtt_reviews3 %>% anti_join(custom_stop_words)
+      
 
-#creating unigrams
-tidy_mtt <- mtt_df %>% unnest_tokens(word,mtt)
+    # write the combined data frame to a CSV file
+      write_csv(mtt_reviews, output_file4)
+      write_csv(mtt_reviews2, output_file5)
+      write_csv(mtt_reviews3, output_file6)
 
-#loading dataset called stop_words
-data("stop_words")
-
-#include custom stop words
-custom_stop_words <- bind_rows(tibble(word = c("app","apps","apple","ipad","aac","touchchat","proloquo2go","ve",
-                                               "mytalktools","mytalk","don","too","iphone","day","pts","lot","lot",
-                                               "itunes","it's","i've","ipod","2","000","20","10","3","5"),
-                                      lexicon = c("custom")),
-                               stop_words)
-#removing all stop words
-tidy_mtt <- tidy_mtt %>% anti_join(custom_stop_words)
 
 #word count
 tidy_mtt %>% count(word, sort = TRUE) 
+tidy_mtt2 %>% count(word, sort = TRUE) 
+tidy_mtt3 %>% count(word, sort = TRUE) 
+mtt_reviews %>% count(word, sort = TRUE) 
+mtt_reviews2 %>% count(word, sort = TRUE) 
+mtt_reviews3 %>% count(word, sort = TRUE) 
+
+
 
 #frequency graph
 tidy_mtt %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 0) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(n, word)) +
+  geom_col() +
+  labs(y = NULL)
+tidy_mtt2 %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 0) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(n, word)) +
+  geom_col() +
+  labs(y = NULL)
+tidy_mtt3 %>%
   count(word, sort = TRUE) %>%
   filter(n > 1) %>%
   mutate(word = reorder(word, n)) %>%
   ggplot(aes(n, word)) +
   geom_col() +
   labs(y = NULL)
-
-#places data into data frame table
-#mtt_df <- tibble(mtt)
-
-#creating unigrams
-tidy_mtt <- mtt_df %>% unnest_tokens(word,mtt, token = "ngrams", n= 2)
-
-#include custom stop words
-custom_stop_words_b <- bind_rows(tibble(word = c("app","apps","apple"),
-                                        lexicon = c("custom")),
-                                 stop_words)
-
-#removing all stop words
-tidy_mtt <- tidy_mtt %>% anti_join(custom_stop_words_b)
-
-#word count
-tidy_mtt %>% count(word, sort = TRUE) 
-
-#frequency graph
-tidy_mtt %>%
-  count(word, sort = TRUE) %>%
-  filter(n > 0) %>%
-  mutate(word = reorder(word, n)) %>%
-  ggplot(aes(n, word)) +
-  geom_col() +
-  labs(y = NULL)
-
-#creating unigrams
-tidy_mtt <- mtt_df %>% unnest_tokens(word,mtt, token = "ngrams", n= 3)
-
-#include custom stop words
-custom_stop_words_c <- bind_rows(tibble(word = c("app","apps","apple"),
-                                        lexicon = c("custom")),
-                                 stop_words)
-
-#removing all stop words
-tidy_mtt <- tidy_mtt %>% anti_join(custom_stop_words_c)
-
-#word count
-tidy_mtt %>% count(word, sort = TRUE) 
-
-#frequency graph
-tidy_mtt %>%
-  count(word, sort = TRUE) %>%
-  filter(n > 0) %>%
-  mutate(word = reorder(word, n)) %>%
-  ggplot(aes(n, word)) +
-  geom_col() +
-  labs(y = NULL)
-
-
-# set the directory containing the files
-file_dir <- "data/MyTalkTools/reviews/"
-
-# get a list of file names in the directory
-file_paths <- list.files(file_dir, full.names = TRUE)
-
-#loading dataset called stop_words
-data("stop_words")
-
-# read in the files and combine the data
-mtt_reviews <- map(file_paths, read_file) %>% 
-  # combine the text into a single character vector
-  reduce(c) %>% 
-  # convert to a data frame
-  tibble(text = .) %>% 
-  # separate the text into words
-  unnest_tokens(word, text)
-
-# set the file path for the output file
-output_file <- "data/MyTalkTools/mtt.csv"
-
-# write the combined data frame to a CSV file
-write_csv(mtt_reviews, output_file)
-
-# read in the files and combine the data
-mtt_reviews2 <- map(file_paths, read_file) %>% 
-  # combine the text into a single character vector
-  reduce(c) %>% 
-  # convert to a data frame
-  tibble(text = .) %>% 
-  # separate the text into words
-  unnest_tokens(word, text, token = "ngrams", n= 2)
-
-# set the file path for the output file
-output_file2 <- "data/MyTalkTools/mtt2.csv"
-
-# write the combined data frame to a CSV file
-write_csv(mtt_reviews2, output_file2)
-
-# read in the files and combine the data
-mtt_reviews3 <- map(file_paths, read_file) %>% 
-  # combine the text into a single character vector
-  reduce(c) %>% 
-  # convert to a data frame
-  tibble(text = .) %>% 
-  # separate the text into words
-  unnest_tokens(word, text, token = "ngrams", n= 3)
-
-# set the file path for the output file
-output_file3 <- "data/MyTalkTools/mtt3.csv"
-
-# write the combined data frame to a CSV file
-write_csv(mtt_reviews3, output_file3)
-
-
-#include custom stop words
-custom_stop_words <- bind_rows(tibble(word = c("app","apps","apple","ipad","aac","MyTalkTools","proloquo2go","ve",
-                                               "mytalktools","mytalk","don","too","iphone","day","pts","lot","lot",
-                                               "itunes","it's","i've","ipod","2","000","20","10","3","5"),
-                                      lexicon = c("custom")),
-                               stop_words)
-
-#removing all stop words
-mtt_reviews <- mtt_reviews %>% anti_join(custom_stop_words)
-mtt_reviews2 <- mtt_reviews2 %>% anti_join(custom_stop_words)
-mtt_reviews3 <- mtt_reviews3 %>% anti_join(custom_stop_words)
-
-#word count
-mtt_reviews %>% count(word, sort = TRUE) 
-mtt_reviews2 %>% count(word, sort = TRUE) 
-mtt_reviews3 %>% count(word, sort = TRUE) 
-
-
-#frequency graph
 mtt_reviews %>%
   count(word, sort = TRUE) %>%
  filter(n > 2) %>%
